b4200f3e05da61c6799c0b4384534dae499bd1c0
Angelina Kosenko <73019553+Angelina-Kosenko@users.noreply.github.com>
2020-12-10 22:49:40 +0200

    Test 7

---
+++ PMAcademy/Homework/Task 1.swift
@@ -0,0 +1,27 @@
+//
+//  Task 1.swift
+//  PMAcademy
+//
+//  Created by Ангелина Косенко on 10.12.2020.
+//
+
+import Foundation
+
+let x = 1.23556789
+let y = Double(round(Double(1000*x))/1000)
+print(y)
+
+//
+
+func pi(to n: Int) -> Double {
+        let factor = (pow(10, n) as NSDecimalNumber).doubleValue
+        let piResult: Double = Double(round(Double.pi * factor) / factor)
+        if(n < 1 || n > 14){
+            print("Please input range from 1 to 14")
+        }else{
+            print(piResult)
+        }
+        return piResult
+    }
+
+  pi(to: 5) 
---
+++ PMAcademy/Homework/Task 3.swift
@@ -0,0 +1,20 @@
+//
+//  Task 3.swift
+//  PMAcademy
+//
+//  Created by Ангелина Косенко on 10.12.2020.
+//
+
+import Foundation
+
+func factorial(of num: Int) -> Int {
+    if num == 1 {
+        return 1
+    } else {
+        return num * factorial(of:num - 1)
+    }
+}
+
+let x = 5
+let result = factorial(of: x)
+print("The factorial of \(x) is \(result)") 
