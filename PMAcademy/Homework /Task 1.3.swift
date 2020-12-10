b0d85bfa535fa121dc34aac08867ab4b57c41182
Angelina Kosenko <73019553+Angelina-Kosenko@users.noreply.github.com>
2020-12-10 22:19:58 +0200

    Test 3

---
+++ PMAcademy/Homework /Task 1.3.swift
@@ -0,0 +1,20 @@
+//
+//  Task 1.3.swift
+//  PMAcademy
+//
+//  Created by Ангелина Косенко on 10.12.2020.
+//
+
+import Foundation
+import Foundation
+
+func factorial(y: Int) {
+    var x = 1
+    for i in 1...y {
+        x = x * i
+
+    }
+
+    
+    print (x)
+}
